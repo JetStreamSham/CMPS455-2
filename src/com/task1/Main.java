a32c3dd2d7eb1f9ab2a45b2825678f78c29a4ea5
JetStream <76616771+JetStreamSham@users.noreply.github.com>
2022-03-06 20:50:58 -0600

    joined all projects

---
+++ src/com/task1/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.concurrent.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Generate number of domains/objects
+        int numObj = (int)(Math.random() * 5 + 3);
+        int numDomains = (int)(Math.random() * 5 + 3);
+
+        AccessMatrix myMatrix = new AccessMatrix(numObj, numDomains);
+        System.out.println("This run's access matrix is: ");
+        System.out.println(myMatrix.toString());
+
+        //Generate objects
+        Object[] objects = new Object[numObj];
+        for(int i = 0; i < objects.length; i++){
+            objects[i] = new Object("Object" + ( i + 1));
+        }
+
+        //Generate users
+        User[] users = new User[numDomains];
+        for(int i = 0; i < numDomains; i++){
+            users[i] = new User("User" + (i + 1), myMatrix, i, objects);
+        }
+
+        //Run each user 5 times.
+        for(int i = 0; i < 5; i++){
+            for(User j : users){
+                    j.run();
+            }
+        }
+        System.out.println();
+        System.out.println("FINAL STATE OF OBJECTS: ");
+        for(Object i : objects){
+            System.out.println(i.name + " says: " + i.buffer);
+        }
+
+        System.out.println("Done!");
+    }
+}
---
+++ src/com/task1/User.java
@@ -0,0 +1,112 @@
+package com.company;
+import java.util.concurrent.*;
+import java.util.Random;
+
+public class User implements Runnable {
+
+    Thread myThread;
+    AccessMatrix myAccess;
+    Object[] listOfObjects;
+    public int myDomain;
+    String myString;
+    //False means we will read from an object: true means we will write to it
+    IOPermission ReadWriteDecision;
+    int randObj;
+    int requestCount = 5;
+    int numCompletions = 0;
+
+    public User(String threadname, AccessMatrix inAM, int inDomain, Object[] inList){
+        myThread = new Thread(this,threadname);
+        myAccess = inAM;
+        myDomain = inDomain;
+        myString = threadname + " was here! ";
+        listOfObjects = inList;
+        System.out.println(threadname + " instantiated!");
+        myThread.start();
+    }
+
+    public void run(){
+
+        //Decide what operation, and on who:
+        randObj = (int)(Math.random() * myAccess.matrix[0].length);
+
+        if(randObj < myAccess.matrix[0].length - myAccess.matrix.length){
+            Random rd = new Random();
+            boolean rand = rd.nextBoolean();
+
+            if(rand){
+                ReadWriteDecision = IOPermission.R;
+                System.out.println(myThread.getName() + " will read from Object " + (randObj+1) + ", permission is: " + myAccess.matrix[myDomain][randObj].name());
+            }
+            else{
+                ReadWriteDecision = IOPermission.W;
+                System.out.println(myThread.getName() + " will write to Object " + (randObj+1) + ", permission is: " + myAccess.matrix[myDomain][randObj].name());
+
+            }
+        }
+        else if(randObj >= myAccess.matrix[0].length - myAccess.matrix.length){
+            System.out.println(myThread.getName() + " will switch to Domain " + (((myAccess.matrix.length - myAccess.matrix[0].length) + randObj) + 1) + ", permission is: " + myAccess.matrix[myDomain][randObj].name());
+        }
+
+        Thread.yield();
+
+        //Check for permission:
+
+        boolean hasPermission = Arbitrate(this);
+
+        //Perform operation
+        if(hasPermission){
+
+            if(randObj < myAccess.matrix[0].length - myAccess.matrix.length){
+                if(ReadWriteDecision == IOPermission.R){
+                    listOfObjects[randObj].Read(this);
+                }
+                else if(ReadWriteDecision == IOPermission.W){
+                    listOfObjects[randObj].Write(this);
+                }
+            }
+            else if(randObj >= myAccess.matrix[0].length - myAccess.matrix.length){
+                myDomain = (myAccess.matrix.length - myAccess.matrix[0].length) + randObj;
+                System.out.println(myThread.getName() + " has switched to Domain " + ((myAccess.matrix.length - myAccess.matrix[0].length) + randObj + 1));
+            }
+
+        }
+        Thread.yield();
+
+        numCompletions++;
+        if(numCompletions < requestCount){
+
+        }
+        //Done!
+    }
+
+
+
+
+
+    public boolean Arbitrate(User user){
+        boolean isPermitted = false;
+
+        if(randObj < myAccess.matrix[0].length - myAccess.matrix.length){
+            if(myAccess.matrix[myDomain][randObj] == ReadWriteDecision || myAccess.matrix[myDomain][randObj] == IOPermission.RW){
+                System.out.println(user.myThread.getName() + " IO permission granted!");
+                isPermitted = true;
+            }
+            else{
+                System.out.println(user.myThread.getName() + " IO permission denied!");
+            }
+
+        }
+        else if(randObj >= myAccess.matrix[0].length - myAccess.matrix.length){
+            if(myAccess.matrix[myDomain][randObj] == IOPermission.Switch){
+                System.out.println(user.myThread.getName() + " switch permission granted!");
+                isPermitted = true;
+            }
+            else{
+                System.out.println(user.myThread.getName() + " switch permission denied!");
+            }
+        }
+
+        return isPermitted;
+    }
+}
